3b61ca549852dc324c9dc5695421d9b7b453a54b
gbalaji <gadi.balaji@epsilon.com>
2023-12-16 23:50:28 +0530

    pushing 2nd script

---
+++ 01-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Very Good Moring"
+
+
+##Note:
+##variables powerful in shell script
+# We should not give space in between variable and values
+# variable value will be subsisted if we use $ symbol in shell scripitng
