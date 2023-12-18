92bf4d94db61f59bddc9b3227e43dd7a584f199d
gbalaji <gadi.balaji@epsilon.com>
2023-12-18 13:43:36 +0530

    pushing script

---
+++ 11-install-packages.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started at exeucint $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE() 
+ {
+ if [ $1 -ne 0 ]
+  then
+    echo -e "$2 ... $R FAILED $N"
+  else
+    echo -e "$2 ... $G SUCCESS $N"
+ fi
+ }
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+#echo "All arguments are passed: $@"
+
+for each_package in $@
+   do
+     yum list installed $each_package &>> $LOGFILE
+     if [ $? -ne 0 ]
+     then 
+         yum install $each_package -y &>> $LOGFILE
+         VALIDATE $? "Installation of $each_package"
+    else
+       echo -e "$package already installed  ... $Y skipping $N"
+    fi
+done
+
+
+
+
