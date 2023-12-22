65f4e0fc4c84b0c0b6f8b88ad168934bc71a78ff
gbalaji <gadi.balaji@epsilon.com>
2023-12-23 04:24:22 +0530

    pushing scripts - 04-datatypes.sh

--- 04-datatypes.sh
+++ 04-datatypes.sh
@@ -3,6 +3,14 @@
 NUMBER1=$1
 NUMBER2=$2
 
+if [ $# -ne 2 ]
+then 
+    echo "Usage: <script name> <arg1> <arg2>"
+    exit 1
+else 
+    echo "please run the script by passing command line args arg1 arg2"
+fi
+
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "Total:: $SUM"
