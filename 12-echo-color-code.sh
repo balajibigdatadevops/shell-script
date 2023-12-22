30c9faa84752a523404f85ed35c0a9e2e5b08c0a
gbalaji <gadi.balaji@epsilon.com>
2023-12-23 04:41:05 +0530

    pushing scripts

---
+++ 12-echo-color-code.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "displaying message different codes"
+
+echo -e "Hello Balaji, I am learning \e[31m shell \e[0m scripting"
+
+echo -e "Hello Balaji, I am learning \e[32m shell \e[0m scripting"
+
+echo -e "Hello Balaji, I am learning \e[33m shell \e[0m scripting"
+
+echo -e "Hello Balaji, I am learning \e[34m shell \e[0m scripting"
+
+echo -e "Hello Balaji, I am learning \e[35m shell \e[0m scripting"
+
+echo -e "Hello Balaji, I am learning \e[36m shell \e[0m scripting"
+
