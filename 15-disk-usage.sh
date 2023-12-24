eb67d6618b71aebefe1e568ffbeea1e37d750d68
gbalaji <gadi.balaji@epsilon.com>
2023-12-25 00:15:29 +0530

    pushing scripts

---
+++ 15-disk-usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT |grep -vE "tmp|File")
+DISK_THRESHOLD=1
+MESSAGE=""
+
+while IFS= read -r LINE
+do
+  Usage=$(echo $LINE | awk '{print $6}'| cut -d "%" -f1)
+  Partition=$(echo $LINE | awk '{print $1}')
+  if [ $Usage -ge $DISK_THRESHOLD ]
+    then 
+     message+="High Disk Usage on $Partition: Usage"
+  fi     
+done <<< $DISK_USAGE
+
+echo "Messaage: $message"
