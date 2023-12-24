4c7bcfd903401e9797c3b8fe7435e4b5593a25c3
gbalaji <gadi.balaji@epsilon.com>
2023-12-24 23:08:43 +0530

    pushing scripts

---
+++ 13-delete-old-log-files.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+SOURCE_DIR="/tmp/shellscript-logs"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE=$($0-$TIMESTAMP).log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $SOURCE_DIR ]
+  then 
+      echo -e "$G creating directory $SOURCE_DIR $N"
+      mkdir -p /tmp/shellscript-logs &>> $LOGFILE
+  else
+      echo -e "$Y directory $SOURCE_DIR exists $N"
+fi
+
+##creating old log files using touch -d command
+
+for Old_log_file in $SOURCE_DIR
+do
+   touch -d 20231201 $Old_log_file/user.log  &>> $LOGFILE
+   touch -d 20231208 $Old_log_file/shipping.log &>> $LOGFILE
+   touch -d 20231221 $Old_log_file/shipping.js &>> $LOGFILE
+   touch -d 20231204 $Old_log_file/user.js &>> $LOGFILE
+done
+
+FILES_TO_DELETE=$(find $SOURCE_DIR  -type f  -name "*.log" -mtime +14)
+
+while IFS= read -r LINE
+do
+  echo "deleting old log files: $LINE" 
+done <<<  $FILES_TO_DELETE
+
+
+
+
---
+++ 14-ifs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+file="/etc/passwd"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ ! -f $file ]
+then 
+   echo -e "$R file:: $file does not exists $N"
+else
+   echo -e "$Y file: $file does exists$N"
+fi
+
+while IFS=":" read -r username password user_id group_id user_fullname user_homedir user_shell_path
+  do
+   echo -e  "user name is: $Y $username $N"
+   echo -e  "password is: $Y $password $N"
+   echo -e  "user_id is: $Y $user_id $N"
+   echo -e "group_id is:$Y $group_id $N"
+   echo -e "user_fullname is:$Y $user_fullname $N"
+   echo -e "user_fullname is:$Y $user_fullname $N"
+   echo -e "user_homedir is:$Y $user_homedir $N"
+   echo -e "user_shell_path is: $Y $user_shell_path $N"
+done <$file
+
