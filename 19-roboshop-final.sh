c36b518a5576ccbd8f366a314c0ee9fa27749b68
gbalaji <gadi.balaji@epsilon.com>
2023-12-28 20:34:33 +0530

    pushing scripts

--- 19-roboshop-final.sh
+++ 19-roboshop-final.sh
@@ -19,9 +19,12 @@ instance_route53_func()
     If [ $PRIVATE == "PrivateIpAddress" ]
 	then
 	IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].$Private' --output text)
-    echo "Displaying Private address"
+	
+	Take_private_IP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[PrivateIpAddress, PublicIpAddress]' --output text | awk -F " " '{print $1}')
+	
+	echo "Displaying Private address"
     echo "$each_address: $IP_ADDRESS"
-
+    
     #create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
@@ -35,13 +38,17 @@ instance_route53_func()
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : "'$IP_ADDRESS'"
+                "Value"         : "'$Take_private_IP'"
             }]
         }
         }]
     }
 	else 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].$Public' --output text)
+	
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].$Private' --output text)
+	
+	Take_public_IP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[PrivateIpAddress, PublicIpAddress]' --output text | awk -F " " '{print $2}')
+	
 	echo "Displaying Public address"
     echo "$i: $IP_ADDRESS"
 
@@ -58,7 +65,7 @@ instance_route53_func()
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : "'$IP_ADDRESS'"
+                "Value"         : "'$Take_public_IP'"
             }]
         }
         }]
