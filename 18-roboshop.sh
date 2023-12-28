3b912ff16585e579b9f12d83c9ab3e61c40380b7
gbalaji <gadi.balaji@epsilon.com>
2023-12-28 17:38:14 +0530

    pushing scripts

--- 18-roboshop.sh
+++ 18-roboshop.sh
@@ -3,8 +3,9 @@
 
 AMI_ID=ami-03265a0778a880afb
 SG_ID=sg-002c9cdb8a04c9b4c
-INSTANCES=("mongo" "redis" "mysql" "rabbitmq" "user" "catalogue" "user" "cart" "shipping" "payment" "web")
+INSTANCES=("mongo" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 ZONE_ID=Z0216330F2XRXARU66P1
+DOMAIN_NAME"balajibigdatadevops.online"
 
 for each_instance in "${INSTANCES[@]}"
   do
@@ -17,6 +18,8 @@ for each_instance in "${INSTANCES[@]}"
           INSTANCE_TYPE="t2.micro"
      fi       
  
-    aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$each_instance}]" --query 'Instances[0].PrivateIpAddress' --output text
-
+    PRIVATE_IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$each_instance}]" --query 'Instances[0].PrivateIpAddress' --output text)
+   
+   aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID  --change-batch '{ "Comment": "creating a record set", "Changes": [ { "Action": "CREATE","ResourceRecordSet": { "Name": "$each_instance.$DOMAIN_NAME", "Type": "A", "TTL": 1, "ResourceRecords": [ { "Value": "$PRIVATE_IP_ADDRESS" } ] } } ] }'
+   
  done 
