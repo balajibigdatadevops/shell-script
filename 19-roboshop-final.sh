30a4108eee047bcbc90643b0fda96b5160fbf128
gbalaji <gadi.balaji@epsilon.com>
2023-12-29 06:54:32 +0530

    pushing scripts

--- 19-roboshop-final.sh
+++ 19-roboshop-final.sh
@@ -17,16 +17,13 @@ do
     fi
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-
-    ROUTE_53_RECORDS=("mongo" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
-    for each_instance in "${ROUTE_53_RECORDS[@]}"
-    do
-    if [ $each_instance != "web" ]
-    then
-
-    	TAKE_PRIVATE_IP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[PrivateIpAddress, PublicIpAddress]' --output text | awk -F " " '{print $1}')
-	    echo "Rout53 records Instance Name $ROUTE_53_RECORDS: PrivateIp is $TAKE_PRIVATE_IP"	
-            #create R53 record, make sure you update record if A record already there
+   
+    for each_instance in "mongo" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web"
+        do
+        if [ $each_instance != "web" ]
+        then
+        	TAKE_PRIVATE_IP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[PrivateIpAddress, PublicIpAddress]' --output text | awk -F " " '{print $1}')
+	        #create R53 record, make sure you update record if A record already there
             aws route53 change-resource-record-sets \
             --hosted-zone-id $ZONE_ID \
             --change-batch '
@@ -47,10 +44,7 @@ do
             '
     else 
     TAKE_PUBLIC_IP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].[PrivateIpAddress, PublicIpAddress]' --output text | awk -F " " '{print $2}')
-	
-	    echo "instance Name $ROUTE_53_RECORDS: PublicIp is $TAKE_PUBLIC_IP"	
-    
-            #create R53 record, make sure you delete existing record
+     #create R53 record, make sure you delete existing record
             aws route53 change-resource-record-sets \
             --hosted-zone-id $ZONE_ID \
             --change-batch '
@@ -59,7 +53,7 @@ do
                 ,"Changes": [{
                 "Action"              : "UPSERT"
                 ,"ResourceRecordSet"  : {
-                    "Name"              : "'$ROUTE_53_RECORDS'.'$DOMAIN_NAME'"
+                    "Name"              : "'$each_instance'.'$DOMAIN_NAME'"
                     ,"Type"             : "A"
                     ,"TTL"              : 1
                     ,"ResourceRecords"  : [{
@@ -69,6 +63,6 @@ do
                 }]
                 }
             '
-    fi
+     fi
     done
 done
