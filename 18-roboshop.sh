7c04af1d87b5a9233725d44e3a658af5adf8f7a7
gbalaji <gadi.balaji@epsilon.com>
2023-12-29 12:50:16 +0530

    pushing script

--- 18-roboshop.sh
+++ 18-roboshop.sh
@@ -7,28 +7,35 @@ INSTANCES=("mongo" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shippin
 ZONE_ID=Z0216330F2XRXARU66P1
 DOMAIN_NAME="balajibigdatadevops.online"
 
-route_53_func() 
-{
-      if 
-      aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID  --change-batch '{ "Comment": "creating a record set", "Changes": [ { "Action": "UPSERT","ResourceRecordSet": { "Name": "'$each_instance'.'$DOMAIN_NAME'", "Type": "A", "TTL": 1, "ResourceRecords": [ { "'Value'": "'$PRIVATE_IP'" } ] } } ] }'
-
-}
-
-for each_instance in "${INSTANCES[@]}"
-  do
-     echo "instance name is ${each_instance}"
-   
-     if [ $each_instance == "mongo" ] || [ $each_instance == "shipping" ] || [ $each_instance == "mysql" ] 
-       then
-          INSTANCE_TYPE="t3.small"
-        elif [ $each_instance == "web" ]
-        
-        PUBLIC_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$each_instance}]"  --query 'Reservations[*].Instances[*].[PublicIpAddress]' --output text )
-
-       else
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
         INSTANCE_TYPE="t2.micro"
-        PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$each_instance}]" --query 'Instances[0].PrivateIpAddress' --output text)
-     fi       
+    fi
 
-  
- done 
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
+done
